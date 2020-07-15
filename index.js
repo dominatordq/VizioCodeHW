93f96e40c934ace289d0a643082206d367ab9d05
dominatordq <dominatordq@gmail.com>
2020-07-15 16:35:01 -0600

    Sorted image array, outputted as img tags in index.html

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 let fetchUrl = require("fetch").fetchUrl;
 let jsdom = require('jsdom');
 let dom = new jsdom.JSDOM();
-let window = dom.window;
-let document = window.document;
+var window1 = dom.window;
+var document1 = window1.document;
 let fs = require("fs");
-var sizeOf = require('image-size');
+let sizeOf = require('image-size');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-let $ = require('jquery')(window);
+let $ = require('jquery')(window1);
 console.log('version:', $.fn.jquery);
 
 let regexp = /<img[^>]+src\s*=\s*['"]([^'"]+)['"][^>]*>/g; // regular experssion for img tags
@@ -20,25 +23,9 @@ let Image = function(id, desc, height, width, url) {
 }
 
 let imgArray = new Array();
-/*
-function getMeta(url){
-    $("<img/>",{
-        on : function(){
-            alert(this.width+' '+this.height);
-        },
-        src : url
-    });
-}
-*/
+let alreadyDisplayed = true;
 
 //let fs = require('fs'), request = require('request');
-
-let download = function(url, filename, callback) {
-    request.head(url, function(err, res, body) {
-        console.log('content-type', res.headers['content-type']);
-        request(url).pipe(fs.createWriteStream(filename)).on('close', callback);
-    });
-}
 
 fetchUrl("https://www.vizio.com/en/smartcast", function(error, meta, body) {
     let htmlBody = body.toString(); // convert html of website to a string
@@ -55,46 +42,72 @@ fetchUrl("https://www.vizio.com/en/smartcast", function(error, meta, body) {
         if ($content.attr('src') !== undefined) {
             let url = "https://www.vizio.com" + $content.attr('src');
             let fileName = url.slice(url.lastIndexOf('/') + 1);
-            let filedata = fs.readFileSync(__dirname + '\\images\\' + fileName);
-            let dimensions = sizeOf(filedata);
+            let filedata = fs.readFileSync(__dirname + '\\images\\' + fileName); // read file to get dimensions
+            let dimensions = sizeOf(filedata); // get dimensions
             //console.log(fileName);            
-            imgArray.push(new Image(i++, fileName, dimensions.height, dimensions.width, url));
-            console.log(imgArray[imgArray.length-1].id);
-            console.log(imgArray[imgArray.length-1].desc);
-            console.log(dimensions.width + 'X' + dimensions.height);
+            imgArray.push(new Image(i++, fileName, dimensions.height, dimensions.width, url)); // push this image to the array of objs
+            //console.log(imgArray[imgArray.length-1].id);
+            //console.log(imgArray[imgArray.length-1].desc);
+            //console.log(dimensions.width + 'X' + dimensions.height);
 
             //getMeta(url);
         //    imgArray.push(new Image($content.attr('id'), $content.attr('src'), 0, 0, url));
         }
-        
+
+        // if a data-src exists as opposed to a src
         if ($content.attr('data-src') !== undefined) {
             let url = "https://www.vizio.com" + $content.attr('data-src');
             let fileName = url.slice(url.lastIndexOf('/') + 1);
             let filedata = fs.readFileSync(__dirname + '\\images\\' + fileName);
             let dimensions = sizeOf(filedata);
             imgArray.push(new Image(i++, fileName, dimensions.height, dimensions.width, url));
-            console.log(imgArray[imgArray.length-1].id);
-            console.log(imgArray[imgArray.length-1].desc);
-            console.log(dimensions.width + 'X' + dimensions.height);
+            //console.log(imgArray[imgArray.length-1].id);
+            //console.log(imgArray[imgArray.length-1].desc);
+            //console.log(dimensions.width + 'X' + dimensions.height);
             //console.log(url);
         }
-
         //console.log(i, img);
         //console.log('id:', $content.attr('id'));
-        //i++;
     } 
 
-    let folder = "images/";
-    // add every image
-    $.ajax({
-        url : folder,
-        success: function (data) {
-            $(data).find("a").attr("href", function (i, val) {
-                if( val.match(/\.(jpe?g|png|gif|svg)$/) ) { 
-                    $("body").append( "<img src='"+ folder + val +"'>" );
-                } 
+    // sort function from smallest to greatest height
+    imgArray.sort(function(a, b) {
+        if (a.height - b.height !== 0)
+            return a.height - b.height;
+        return a.width - b.width;
+    })
+    console.log(imgArray);
+
+    const imageDir = __dirname + "\\images\\";
+
+    // display images on webpage (using a regex -- very sloppy hehe)
+    if (alreadyDisplayed === false) {
+        let divPos = 0;
+            
+        fs.readFile("./index.html", 'utf8', function(err, data) {
+            if (err) return console.log(err);
+        
+            let toPrepand = [];
+            // get each image source and append to an array
+            for (let img of imgArray){
+                toPrepand.push("<img src=" + "\"" + ".\\images\\" + img.desc + "\"" + ">");
+            }
+            // convert array of sources to string and write it to index.html
+            data = data.replace(/\<\/div>/g, toPrepand.join('') + '</div>');
+            //console.log(data);
+            fs.writeFile('./index.html', data, 'utf8', function (err) {
+                if (err) return console.log(err);
             });
-        }
-    });
+            
+            
+        });
+        alreadyDisplayed = true;
+    }
+    // open/create express server to display webpage
+    app.use(express.static(__dirname));
+    app.get('/', function (req, res) {
+        res.sendFile(__dirname + '/index.html');
+    })
+    app.listen(port, () => console.log('The server running on Port ' + port));
 
 });
