53debcdb2dd134dd0b0b8df8f8434db8d8b8b6e9
GQSAMSUNG9\ginoq <emailginoquintana@gmail.com>
2020-07-14 23:57:57 -0600

    test

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let jsdom = require('jsdom');
 let dom = new jsdom.JSDOM();
 let window = dom.window;
 let document = window.document;
+let fs = require("fs");
+let readimage = require("readimage");
 
 let $ = require('jquery')(window);
 console.log('version:', $.fn.jquery);
@@ -29,9 +31,8 @@ function getMeta(url){
 }
 */
 
-let fs = require('fs'),
-                request = require('request');
-            
+//let fs = require('fs'), request = require('request');
+
 let download = function(url, filename, callback) {
     request.head(url, function(err, res, body) {
         console.log('content-type', res.headers['content-type']);
@@ -42,20 +43,35 @@ let download = function(url, filename, callback) {
 fetchUrl("https://www.vizio.com/en/smartcast", function(error, meta, body) {
     let htmlBody = body.toString(); // convert html of website to a string
     //console.log(htmlBody);
-    //console.log(htmlBody.match(regexp));
+    console.log(htmlBody.match(regexp));
     let images = htmlBody.match(regexp); // match all the img tags and return
     let i = 0;
     
     // iterate through each image
     for (let img of images) {
         let $content = $($.parseHTML(img)); // convert string to html so jquery can parse it
-        $content.attr('id', i); // add and id to each image
+        $content.attr('id', i); // add an id to each image
         // if a src exists
         if ($content.attr('src') !== undefined) {
             let url = "https://www.vizio.com" + $content.attr('src');
             //console.log(url);
+            //console.log($content.attr('src'));
             let width, height;
-
+            let fileName = url.slice(url.lastIndexOf('/') + 1);
+            if (!fileName.includes(".svg")) {
+                var filedata = fs.readFileSync(__dirname + '\\images\\' + fileName);
+                //var filedata = fs.readFileSync("C:/Users/ginoq/Documents/VizioCodeHW/images/2020-v5-series-v585-h1-v585-h11-updated.png");
+                readimage(filedata, function (err, image) {
+                    if (err) {
+                        console.log("failed to parse the image")
+                        console.log(err)
+                    }
+                    console.log(fileName);
+                    console.log(image.width + 'X' + image.height);
+                });
+            } else {
+                console.log("Skipping .svg for now");
+            }
             // create an img tag from the url, display width and height
             
             //getMeta(url);
